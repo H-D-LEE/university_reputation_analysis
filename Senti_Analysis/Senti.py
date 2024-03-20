16e5db94c162d07c8a9959e140207c74fd191bbc
MalSook <sunkeuy@gmail.com>
2024-03-20 20:12:08 +0900

    Senti 클래스 모듈화

--- Senti_Analysis/Senti.py
+++ Senti_Analysis/Senti.py
@@ -1,15 +1,18 @@
 import json 
 class Senti():
-    def data_list(wordname):	
-            with open('data/SentiWord_info.json', encoding='utf-8-sig', mode='r') as f:
-                data = json.load(f)
-            result = ['None','None']	
-            for entry in data:
-                if entry['word'] == wordname:
-                    result = [entry['word_root'], entry['polarity']]
-                    break
+    def __init__(self, dic_file):
+        self.dic_file = dic_file
+
+    def data_list(self, wordname):	
+        with open(self.dic_file, encoding='utf-8-sig', mode='r') as f:
+            data = json.load(f)
+        result = ['None','None']	
+        for entry in data:
+            if entry['word'] == wordname:
+                result = [entry['word'], entry['polarity']]
+                break
                 
-            r_word = result[0]
-            s_word = result[1]	
+        word = result[0]
+        pol = result[1]	
             
-            return r_word, s_word
+        return word, pol
--- Senti_Analysis/senti_module.py
+++ Senti_Analysis/senti_module.py
@@ -1,24 +1,8 @@
 import json
 import kss
 from konlpy.tag import Okt
+from Senti import Senti
 
-class Senti():
-    def __init__(self, dic_file):
-        self.dic_file = dic_file
-
-    def data_list(self, wordname):	
-        with open(self.dic_file, encoding='utf-8-sig', mode='r') as f:
-            data = json.load(f)
-        result = ['None','None']	
-        for entry in data:
-            if entry['word'] == wordname:
-                result = [entry['word'], entry['polarity']]
-                break
-                
-        word = result[0]
-        pol = result[1]	
-            
-        return word, pol
 
 def analyze_sentiments(txt, senti_dic):
     okt = Okt()
